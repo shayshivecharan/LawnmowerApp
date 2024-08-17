e7c5abd754e7de88611c51a321ffa4b7a8469a6e
Shailendra Shivecharan <switchcodesolutions@gmail.com>
2024-08-17 17:58:27 -0400

    Add project files.

---
+++ LawnmowerApp/Mower.cs
@@ -0,0 +1,52 @@
+﻿namespace LawnmowerApp;
+
+public class Mower(
+    int x,
+    int y,
+    int maxX,
+    int maxY,
+    int orientation
+)
+{
+    private (int, int) velocity = (velocities[orientation].Item1, velocities[orientation].Item2);
+
+    private static List<(int, int)> velocities = new List<(int, int)>()
+    {
+        { (0, 1) },
+        { (1, 0) },
+        { (0, -1) },
+        { (-1, 0) },
+    };
+
+    public (int finalX, int finalY, int orientation) ProcessCommands(char[] commands)
+    {
+        foreach(var command in commands)
+        {
+            if(command == 'F')
+            {
+                Move();
+            }
+            else if(command == 'L') {
+                orientation = orientation > 0 ? orientation - 1 : 3;
+                velocity = velocities[orientation];
+            }
+            else if (command == 'R')
+            {
+                orientation = orientation < 3 ? orientation + 1 : 0;
+                velocity = velocities[orientation];
+            }
+            else
+            {
+                throw new ArgumentException("Invalid command.");
+            }
+        }
+
+        return (x, y, orientation);
+    }
+
+    private void Move()
+    {
+        x += x == maxX ? 0 : velocity.Item1;
+        y += y == maxY ? 0 : velocity.Item2;
+    }
+}
---
+++ LawnmowerApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using LawnmowerApp;
+
+var lines = File.ReadAllLines("input.txt");
+
+try
+{
+    var max = lines[0].Split(" ").Select(x => int.Parse(x)).ToArray();
+
+    for(var x = 1; x < lines.Length; x += 2)
+    {
+        string[] mowerInit = lines[x].Split(" ");
+        char[] mowerCommands = lines[x + 1].ToCharArray();
+
+        (int finalX, int finalY, int orientation) = new Mower(
+            int.Parse(mowerInit[0]),
+            int.Parse(mowerInit[1]),
+            max[0],
+            max[1],
+            (int) Enum.Parse<Orientations>(mowerInit[2])).ProcessCommands(mowerCommands);
+
+        Console.WriteLine($"{finalX} {finalY} {(Orientations)orientation}");
+    }
+
+    Console.WriteLine("Press any key to end...");
+    Console.ReadKey();
+}
+catch(Exception ex)
+{
+    throw new ArgumentException("Invalid file format.", ex);
+}
+
+
+enum Orientations
+{
+    N = 0,
+    E = 1,
+    S = 2,
+    W = 4
+}
